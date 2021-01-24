600c23eff8a9a8f3aa879cd18950c9e4eb6bdd5f
swati-mca2020 <swati.narang@splashlearn.com>
2021-01-24 19:44:55 +0530

    first commit by swati

---
+++ src/packag2/printinpkg2.java
@@ -0,0 +1,9 @@
+package packag2;
+
+public class printinpkg2 {
+
+    public void printmethodpk2()
+    {
+        System.out.println("this is print of package2");
+    }
+}
---
+++ src/pkg1/pkg1class.java
@@ -0,0 +1,10 @@
+package pkg1;
+
+public class pkg1class {
+
+    public void printpkg1()
+
+    {
+        System.out.println("this is pkg2 print method")
+    }
+}
